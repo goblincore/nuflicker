25e8558371773228d5c0269c68d11a92a58a8099
goblincore <withallmyhrt@gmail.com>
2024-04-18 14:16:56 -0400

    Initial Commit

---
+++ nuflicker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  nuflicker
+//
+//  Created by Donny on 4/18/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
